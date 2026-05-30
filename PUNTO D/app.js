b61fbb9ed1a21b90667822ee963d7df34583a292
JesmaR <duranm.jesusm@gmail.com>
2026-05-30 17:36:09 -0500

    solucion ejercicio d2

--- PUNTO D/app.js
+++ PUNTO D/app.js
@@ -4,14 +4,48 @@ viaje con los siguientes costos fijos:
 • Alojamiento: $200.000
 • Alimentación: $150.000 */
 
-let transporte = 120000;
-let alojamiento = 200000;
-let alimentacion = 150000;
-let total = transporte + alojamiento + alimentacion;
+const TRANSPORTE = 120000;
+const ALOJAMIENTO = 200000;
+const ALIMENTACION = 150000;
+const PERSONAS = 4;
+
+let total = TRANSPORTE + ALOJAMIENTO + ALIMENTACION;
+let aporteIndividual = total / PERSONAS;
+let entrega = 130000;
+let sobrante = entrega - aporteIndividual;
+
 alert("Gastos del viaje:\n" +
-      "Transporte: $" + transporte.toLocaleString() + "\n" +
-      "Alojamiento: $" + alojamiento.toLocaleString() + "\n" +
-      "Alimentación: $" + alimentacion.toLocaleString() + "\n" +
-      "-----------------------------\n" +
-      "Total: $" + total.toLocaleString());
+      "Transporte: $" + TRANSPORTE.toLocaleString() + "\n" +
+      "Alojamiento: $" + ALOJAMIENTO.toLocaleString() + "\n" +
+      "Alimentación: $" + ALIMENTACION.toLocaleString() + "\n" +
+      "-----------------------------------\n" +
+      "Total: $" + total.toLocaleString() + "\n" +
+      "Cada persona debe aportar: $" + aporteIndividual.toLocaleString() + "\n" +
+      "Cada persona entrega: $" + entrega.toLocaleString() + "\n" +
+      "Sobrante por persona: $" + sobrante.toLocaleString());
+
+/*Ejercicio 2: Conversor de tiempo Un aprendiz registró 7.200 segundos de actividad física. 
+Necesita expresarlos en:
+• Minutos.
+• Horas.
+• Días.
+Requerimiento:
+• Usa una constante para los segundos.
+• Declara variables para almacenar las conversiones. */
+
+const SEGUNDOS = 7200;
+
+let minutos = SEGUNDOS / 60;
+let horas = SEGUNDOS / 3600;
+let dias = SEGUNDOS / 86400;
+
+alert("Conversión de tiempo:\n" +
+      SEGUNDOS + " segundos equivalen a:\n" +
+      minutos + " minutos\n" +
+      horas + " horas\n" +
+      dias + " días");
+
+
+
+
 
